637ba6e0e9bb2a85d5f8e4fb97565237959a3da1
seyiiishay <pfowokan2000@gmail.com>
2025-07-05 13:09:30 -0600

    db.php
    It connects you to the database.

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "opo642";
+    $db_pwd = "Elizabeth@24";
+    $db_db = "opo642";
+    $charset = "utf8mb4";
+    
+    $attr="mysql:host=$db_host;dbname=$db_db;charset=$charset";
+
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    function connect_to_db($attr, $db_user, $db_pwd, $options) {
+        try {
+            $db = new PDO($attr, $db_user, $db_pwd, $options);
+            return $db;
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+?>
