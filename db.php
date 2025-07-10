b74daefab9f98f8f3e317dc324dbd3bcaeea95ce
seyiiishay <pfowokan2000@gmail.com>
2025-07-09 18:55:45 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
     $db_host = "localhost";
-    $db_user = "opo642";
-    $db_pwd = "Elizabeth@24";
-    $db_db = "opo642";
+    $db_user = "";
+    $db_pwd = "";
+    $db_db = "";
     $charset = "utf8mb4";
     
     $attr="mysql:host=$db_host;dbname=$db_db;charset=$charset";
