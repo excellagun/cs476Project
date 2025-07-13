3b9de36a78f9cb4c7d4097a9a6fc0168f70bc8f1
excellagun <excellolaoluwa123@gmail.com>
2025-07-12 19:32:12 -0600

    Update eventRegisterSignup.js

--- eventRegisterSignup.js
+++ eventRegisterSignup.js
@@ -1,11 +1,9 @@
-var name = document.getElementById("name");
 var uname = document.getElementById("screenname");
 var pwd = document.getElementById("pass");
 var cpwd = document.getElementById("confirm_pwd");
 var dob = document.getElementById("dob");
 var email = document.getElementById("email");
 
-name.addEventListener("blur", nameHandler);
 uname.addEventListener("blur", usernameHandler);
 pwd.addEventListener("blur", pwdHandler);
 cpwd.addEventListener("blur", cpwdHandler);
